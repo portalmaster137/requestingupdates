5594cb7793f3fcb09fe4d8e7200d4cf67c2575f3
Portalmaster137 <portalmaster137@gmail.com>
2022-11-18 04:45:15 +1000

    update check

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,37 @@ extern crate reqwest;
 
 #[tokio::main]
 async fn main() {
-    match reqwest::get("http://youtube.com").await {
-        Ok(response) => {
-            println!("Status: {}", response.status());
-            println!("Headers:\n{:#?}", response.headers());
+    let version_url =
+        "https://raw.githubusercontent.com/portalmaster137/requestingupdates/main/.version";
+    check_version(version_url.to_string()).await;
+}
+
+fn check_version_is_higher(file_version: String, remote_version: String) -> bool {
+    let file_version = file_version.replace(".", "");
+    let remote_version = remote_version.replace(".", "");
+    let file_version = file_version.parse::<i32>().unwrap();
+    let remote_version = remote_version.parse::<i32>().unwrap();
+    if file_version < remote_version {
+        return true;
+    }
+    return false;
+}
+
+async fn check_version(ver_url: String) {
+    if (std::path::Path::new(".version").exists()) {
+        //load version
+        let version = std::fs::read_to_string(".version").unwrap();
+        println!("Version: {}", version);
+        //get remote version
+        let remote = reqwest::get(ver_url).await.unwrap().text().await.unwrap();
+        println!("Remote Version: {}", remote);
+        //compare versions
+        if check_version_is_higher(version, remote) {
+            println!("Update available!");
+        } else {
+            println!("No update available!");
         }
-        Err(e) => println!("Error: {}", e),
+    } else {
+        panic!("Version file does not exist!, Please reclone the repo");
     }
 }
